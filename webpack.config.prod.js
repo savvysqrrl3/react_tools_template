e8e5597f05353362747485e1249febe66de39585
April Cound <musicwithapril@gmail.com>
2019-01-21 00:56:40 -0800

    eternalized react for prod, chained dev script

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -2,5 +2,9 @@ const merge = require('webpack-merge')
 const baseConfig = require('./webpack.config.base')
 
 module.exports = merge(baseConfig, {
-    mode: 'production'
+    mode: 'production',
+    externals: {
+        react: 'React',
+        'react-dom': 'ReactDOM'
+      }
   })
