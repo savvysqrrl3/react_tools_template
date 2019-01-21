02774263f7f4652e13b2d21b60bb8dfcf576c274
April Cound <musicwithapril@gmail.com>
2019-01-21 00:02:46 -0800

    added hot reloading dev script

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { hot } from 'react-hot-loader'
 
 class App extends React.Component{
     render(){
@@ -6,4 +7,4 @@ class App extends React.Component{
     }
 }
 
-export default App
+export default hot(module)(App)
