29e880adcff7f8e81ae2ac09d5de6c3b1daaed7d
April Cound <musicwithapril@gmail.com>
2019-01-22 18:49:42 -0800

    added prettier for code formatting

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React from 'react'
-import { hot } from 'react-hot-loader'
+import React from 'react';
+import { hot } from 'react-hot-loader';
 
-class App extends React.Component{
-    render(){
-        return <h2>Your new app will be here. Build something cool!</h2>
-    }
+class App extends React.Component {
+  render() {
+    return <h2>Your new app will be here. Build something cool!</h2>;
+  }
 }
 
-export default hot(module)(App)
+export default hot(module)(App);
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,10 +1,10 @@
-const merge = require('webpack-merge')
-const baseConfig = require('./webpack.config.base')
+const merge = require('webpack-merge');
+const baseConfig = require('./webpack.config.base');
 
 module.exports = merge(baseConfig, {
-    mode: 'production',
-    externals: {
-        react: 'React',
-        'react-dom': 'ReactDOM'
-      }
-  })
+  mode: 'production',
+  externals: {
+    react: 'React',
+    'react-dom': 'ReactDOM'
+  }
+});
