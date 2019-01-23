b1a02981b47e220d857d0f18aa72f54a4a11cfe0
April Cound <musicwithapril@gmail.com>
2019-01-22 23:02:50 -0800

    added react-axe to detect accessibility violations

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import '@babel/polyfill';
 import App from './App';
 
+if (process.env.NODE_ENV === 'development') {
+  const axe = require('react-axe');
+  axe(React, ReactDOM, 1500);
+}
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
